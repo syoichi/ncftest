9a497c6d8b5d2ed05c51bccdb0656868464da2c6
Syoichi Tsuyuhara <syoichi@outlook.com>
2012-11-24 02:23:34 +0900

    perspective's length value doesn't contain 0
    >"Lengths must be positive."
    http://dev.w3.org/csswg/css3-transforms/#perspective-property

--- tests.js
+++ tests.js
@@ -292,7 +292,7 @@ window.Specs = {
 			],
 			"transform-origin": ["10px", "top", "top left", "50% 100%", "left 0%", "right bottom 20px"],
 			"transform-style": ["flat", "preserve-3d"],
-			"perspective": ["none", "0", "600px"],
+			"perspective": ["none", "600px"],
 			"perspective-origin": ["10px", "top", "top left", "50% 100%", "left 0%"],
 			"backface-visibility": ["visible", "hidden"],
 		}
