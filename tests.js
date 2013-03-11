62edb605ca1df643a7f6a8fae0e4fe427f5e2dc5
Syoichi Tsuyuhara <syoichi@outlook.com>
2013-03-11 23:27:28 +0900

    Update "Grid Template Layout"

--- tests.js
+++ tests.js
@@ -1153,41 +1153,52 @@ window.Specs = {
 			"grid-template": "\"****\" \"****\" \"****\"",
 			"grid-columns": [
 				"auto", "1px", "10%", "*", "1fr",
-				"minmax(1px , 10px)", "min-content", "max-content", "fit-content",
-				"* * * *"
+				"minmax(1px, 10px)", "min-content", "max-content", "fit-content",
+				"minmax(10%, 10px)", "minmax(*, 10px)", "minmax(1fr, 10px)", "minmax(minmax(1px, 10px), 10px)",
+				"minmax(min-content, 10px)", "minmax(max-content, 10px)", "minmax(fit-content, 10px)",
+				"minmax(min-content, max-content)",
+				"1px 1px", "* * * *"
 			],
 			"grid-rows": [
 				"auto", "1px", "10%", "*", "1fr",
-				"minmax(1px , 10px)", "min-content", "max-content", "fit-content",
-				"* * * *"
+				"minmax(1px, 10px)", "min-content", "max-content", "fit-content",
+				"minmax(10%, 10px)", "minmax(*, 10px)", "minmax(1fr, 10px)", "minmax(minmax(1px, 10px), 10px)",
+				"minmax(min-content, 10px)", "minmax(max-content, 10px)", "minmax(fit-content, 10px)",
+				"minmax(min-content, max-content)",
+				"1px 1px", "* * * *"
 			],
 			"grid": ["none"].concat(
-				[
-					"1px", "10%", "*", "1fr", "minmax(1px , 10px)",
-					"min-content", "max-content", "fit-content", "* * * *"
-				].and(["\"a b c\""]),
 				"\"a b c\"",
+				[
+					"1px", "10%", "*", "1fr",
+					"minmax(1px, 10px)", "min-content", "max-content", "fit-content",
+					"minmax(10%, 10px)", "minmax(*, 10px)", "minmax(1fr, 10px)", "minmax(minmax(1px, 10px), 10px)",
+					"minmax(min-content, 10px)", "minmax(max-content, 10px)", "minmax(fit-content, 10px)",
+					"minmax(min-content, max-content)",
+					"1px 1px", "* * * *"
+				].and(["\"a b c\""]),
 				["\"a b c\""].and([
-					"1px", "10%", "*", "1fr", "minmax(1px , 10px)",
-					"min-content", "max-content", "fit-content", "* * * *"
+					"1px", "10%", "*", "1fr",
+					"minmax(1px, 10px)", "min-content", "max-content", "fit-content",
+					"minmax(10%, 10px)", "minmax(*, 10px)", "minmax(1fr, 10px)", "minmax(minmax(1px, 10px), 10px)",
+					"minmax(min-content, 10px)", "minmax(max-content, 10px)", "minmax(fit-content, 10px)",
+					"minmax(min-content, max-content)"
 				]),
-				"10% \"d e f\" minmax(1px , 10px)",
-				"10% \"d e f\" minmax(1px , 10px) \"g h i\" min-content",
+				"10% \"d e f\" minmax(1px, 10px)",
+				"\"a b c\" * \"d e f\" *",
+				"10% \"d e f\" minmax(1px, 10px) \"g h i\" min-content",
 				"10% \"d e f\" \"g h i\" max-content",
 				"\"a b\" \"c d\"", "* 10em \"a b c\" \"a b d\" 4em"
 			),
 			"flow": ["auto", "p1", "\"initial\"", "*", "same"],
-			"grid-area": ["same", "next"].times(2).concat(
-				["same", "next"].and(["1"]), ["2"].and(["same", "next"]),
-				["same", "next"].times(2).and(["3 4"]),
-				["same", "next"].and(["5"]).and(["6 7"]),
-				["8"].and(["same", "next"]).and(["9 10"])
+			"grid-area": ["none"].concat(
+				["1", "same", "next"].times(2),
+				["1", "same", "next"].times(2).and(["1 1"])
 			),
 			"chains": ["none", "a", "b c", "d, e", "f g, h i", "j, k l", "m n, o p", "@ a b c d e, f g h i j k"]
 		},
 		"selectors": {
-			"::slot()": "body::slot(a)",
-			"::flow()": "p::flow(b)"
+			"::slot()": "body::slot(a)"
 		}
 	},
 	
