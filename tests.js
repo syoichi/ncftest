917d9e2b593b9c57b13b7edb7bbcdb6c6d2b88d9
Syoichi Tsuyuhara <syoichi@outlook.com>
2013-03-12 11:42:33 +0900

    Update "Values and Units"

--- tests.js
+++ tests.js
@@ -933,19 +933,53 @@ window.Specs = {
 		"title": "Values and Units",
 		"values": {
 			"properties": [
-				"width",
-				"padding"
+				"top",
+				"bottom",
+				"left",
+				"right",
+				// "width",
+				// "height",
+				// "max-width",
+				// "max-height",
+				// "min-width",
+				// "min-height",
+				"margin",
+				// "padding",
+				// "font-size",
+				// "line-height",
+				"background-position",
+				"text-indent",
+				"vertical-align"
 			],
 			"initial": "initial",
-			"rem": "5rem",
 			"ch": "5ch",
+			"rem": "5rem",
 			"vw": "5vw",
 			"vh": "5vh",
 			"vmin": "5vmin",
 			"vmax": "5vmax",
-			"attr()": ["attr(data-px)", "attr(data-px px)", "attr(data-px px, initial)"],
-			"calc()": ["calc(1px + 2px)", "calc(5px*2)", "calc(5px/2)", "calc(100%/3 - 2*1em - 2*1px)", /* "calc(attr(data-px)*2)", */ "calc(5px - 10px)"],
-			"toggle()": "toggle(1px, 2px)"
+			"calc()": [
+				/*"calc(1)", "calc(0)", "calc(-1)", "calc(+1)", "calc(1.1)",*/
+				"calc(1px)", "calc(0px)", "calc(-1px)", "calc(+1px)", "calc(1.1px)",
+				"calc(1%)", "calc(0%)", "calc(-1%)", "calc(+1%)", "calc(1.1%)",
+				"calc(1px + 2px)", "calc(5px - 10px)", "calc(5px * 2)", "calc(5px / 2)",
+				"calc(100%/3 - 2*1em - 2*1px)"/*, "calc(attr(data-px) * 2)"*/
+			],
+			"toggle()": ["toggle(1px, 2px)", "toggle(1px, 2px, 3px)"],
+			"attr()": [
+				"data-px",
+				"data-text string", "data-text color", "data-text url", "data-text integer",
+				"data-text number", "data-text length", "data-text angle", "data-text time",
+				"data-text frequency", "data-text em", "data-text ex", "data-text px",
+				"data-text rem", "data-text vw", "data-text vh", "data-text vmin",
+				"data-text vmax", "data-text mm", "data-text cm", "data-text in",
+				"data-text pt", "data-text pc", "data-text deg", "data-text grad",
+				"data-text rad", "data-text ms", "data-text s", "data-text Hz",
+				"data-text kHz", "data-text %",
+				"data-px, 1px", "data-px px, 1px", "data-px px, calc(1px)"
+			].map(function (arg) {
+				return "attr(" + arg + ")";
+			})
 		}
 	},
 	
