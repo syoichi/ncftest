11263d9f4ad493bea92826e6a499941129e7ade8
Syoichi Tsuyuhara <syoichi@outlook.com>
2013-06-07 02:18:52 +0900

    Make supports.js JSHint friendly

--- js/supports.js
+++ js/supports.js
@@ -1,157 +1,199 @@
-(function(){
+(function executeSupports(win, doc) {
+  'use strict';
 
-/**
- * Setup dummy elements
- */
+  var dummy, inline, style, iframe, head, prefixes, prefixesLen;
 
-var dummy = document.createElement('_'),
-    inline = dummy.style,
-    style = document.createElement('style');
+  /**
+   * Setup dummy elements
+   */
+  dummy = doc.createElement('_');
+  inline = dummy.style;
+  style = doc.createElement('style');
 
-// On WebKit, @rule test is too slow.
-if (/WebKit/.test(navigator.userAgent)) {
-  var iframe = document.createElement('iframe');
-  iframe.src = 'about:blank';
-  document.head.appendChild(iframe);
-  iframe.contentDocument.head.appendChild(style);
-} else {
-  document.head.appendChild(style);
-}
+  head = doc.head;
+  prefixes = [
+    '', '-moz-', '-webkit-', '-o-', '-ms-',
+    '-wap-', '-op-', '-xv-', 'ms-', '-khtml-', '-apple-'
+  ];
+  prefixesLen = prefixes.length;
 
-dummy.setAttribute('data-foo', 'bar');
-dummy.setAttribute('data-px', '1px');
-document.head.appendChild(dummy);
-
-var _ = window.Supports = {
-  prefixes: ['', '-moz-', '-webkit-', '-o-', '-wap-', '-op-', '-xv-', '-ms-', 'ms-', '-khtml-', '-apple-'],
-
-  property: function(property) {
-    if(property.charAt(0) === '-') {
-      return camelCase(property) in inline? property : false;
-    }
-
-    if(!_.property.cached) {
-      _.property.cached = {};
-    }
-    else if(_.property.cached[property]) {
-      return _.property.cached[property];
-    }
-
-    for(var i=0; i<_.prefixes.length; i++) {
-      var prefixed = _.prefixes[i] + property;
-
-      if(camelCase(prefixed) in inline) {
-        return _.property.cached[property] = prefixed;
-      }
-    }
-
-    return _.property.cached[property] = false;
-  },
-
-  value: function(property, value, label) {
-    if(!_.value.cached) {
-      _.value.cached = {};
-    }
-    property = _.property(property);
-
-    if(!property) { return false; }
-
-    property = camelCase(property);
-
-    inline.cssText = '';
-    inline[property] = '';
-
-    for(var i=0; i<_.prefixes.length; i++) {
-      var prefixed = _.prefixes[i] + value;
-
-      try {
-        inline[property] = prefixed;
-      } catch(e) {}
-
-      if(inline.length > 0) {
-        if (label && _.value.cached[label] === void 0) {
-          _.value.cached[label] = _.prefixes[i] + label;
-        }
-        return prefixed;
-      }
-    }
-
-    return false;
-  },
-
-  selector: function(selector) {
-    if(!_.selector.cached) {
-      _.selector.cached = {};
-    }
-    else if(_.selector.cached[selector]) {
-      return _.selector.cached[selector];
-    }
-
-    for(var i=0; i<_.prefixes.length; i++) {
-      var prefixed = selector.replace(/^(:+)/, '$1' + _.prefixes[i]);
-
-      try {
-        document.querySelector(prefixed);
-        return  _.selector.cached[selector] = prefixed;
-      }
-      catch (e) {}
-    }
-
-    return  _.selector.cached[selector] = false;
-  },
-
-  atrule: function(atrule, atruleName) {
-    if(!_.atrule.cached) {
-      _.atrule.cached = {};
-    }
-    else if(_.atrule.cached[atrule]) {
-      return _.atrule.cached[atrule];
-    }
-
-    for(var i=0; i<_.prefixes.length; i++) {
-      var prefixed = atrule.replace(/^@/, '@' + _.prefixes[i]);
-
-      style.textContent = prefixed + '{}';  // Safari 4 has issues with style.innerHTML
-
-      if(style.sheet.cssRules.length > 0) {
-        if (_.atrule.cached[atruleName] === void 0) {
-          _.atrule.cached[atruleName] = atruleName.replace(/^@/, '@' + _.prefixes[i]);
-        }
-        return _.atrule.cached[atrule] = prefixed;
-      }
-    }
-
-    return _.atrule.cached[atrule] = false;
-  },
-
-  mq: function(mq, mqName) {
-    if(!_.mq.cached) {
-      _.mq.cached = {};
-    } else if(_.mq.cached[mq]) {
-      return _.mq.cached[mq];
-    }
-
-    for(var i=0; i<_.prefixes.length; i++) {
-      var prefixed = mq.replace(/\(/g, '(' + _.prefixes[i]);
-      var mql = matchMedia(prefixed);
-
-      if(mql.media !== 'invalid' && mql.matches) {
-        if (_.mq.cached[mqName] === void 0) {
-          _.mq.cached[mqName] = _.prefixes[i] + mqName;
-        }
-        return _.mq.cached[mq] = prefixed;
-      }
-    }
-
-    return _.mq.cached[mq] = false;
+  function camelCase(str) {
+    return str.replace(/-([a-z])/g, function makeUpperCase($0, $1) {
+      return $1.toUpperCase();
+    }).replace('-', '');
   }
-};
 
-/**
- * Private
- */
-function camelCase (str) {
-  return str.replace(/-([a-z])/g, function($0, $1) { return $1.toUpperCase(); }).replace('-','');
-}
+  // On WebKit/Blink, @rule test is too slow.
+  if (/WebKit/.test(win.navigator.userAgent)) {
+    iframe = document.createElement('iframe');
+    iframe.src = 'about:blank';
+    head.appendChild(iframe);
+    iframe.contentDocument.head.appendChild(style);
+  } else {
+    head.appendChild(style);
+  }
 
-})();
+  dummy.setAttribute('data-foo', 'bar');
+  dummy.setAttribute('data-px', '1px');
+  head.appendChild(dummy);
+
+  win.Supports = {
+    property: function propertyFunc(property) {
+      var cached, idx, prefixed;
+
+      if (property.charAt(0) === '-') {
+        return camelCase(property) in inline ? property : false;
+      }
+
+      cached = propertyFunc.cached;
+
+      if (!cached) {
+        cached = propertyFunc.cached = {};
+      } else if (cached[property]) {
+        return cached[property];
+      }
+
+      for (idx = 0; idx < prefixesLen; idx += 1) {
+        prefixed = prefixes[idx] + property;
+
+        if (camelCase(prefixed) in inline) {
+          cached[property] = prefixed;
+
+          return prefixed;
+        }
+      }
+
+      cached[property] = false;
+
+      return false;
+    },
+    value: function valueFunc(property, value, label) {
+      var cached, idx, prefix, prefixed;
+
+      cached = valueFunc.cached;
+
+      if (!cached) {
+        cached = valueFunc.cached = {};
+      }
+
+      property = this.property(property);
+
+      if (!property) {
+        return false;
+      }
+
+      property = camelCase(property);
+      inline[property] = inline.cssText = '';
+
+      for (idx = 0; idx < prefixesLen; idx += 1) {
+        prefix = prefixes[idx];
+        prefixed = prefix + value;
+
+        // Internet Explorer 10 throws Undefined Error.
+        try {
+          inline[property] = prefixed;
+        } catch (err) {}
+
+        if (inline.length > 0) {
+          if (label && cached[label] === void 0) {
+            cached[label] = prefix + label;
+          }
+
+          return prefixed;
+        }
+      }
+
+      return false;
+    },
+    selector: function selectorFunc(selector) {
+      var cached, idx, prefixed;
+
+      cached = selectorFunc.cached;
+
+      if (!cached) {
+        cached = selectorFunc.cached = {};
+      } else if (cached[selector]) {
+        return cached[selector];
+      }
+
+      for (idx = 0; idx < prefixesLen; idx += 1) {
+        prefixed = selector.replace(/^(:+)/, '$1' + prefixes[idx]);
+
+        try {
+          doc.querySelector(prefixed);
+
+          cached[selector] = prefixed;
+
+          return prefixed;
+        } catch (err) {}
+      }
+
+      cached[selector] = false;
+
+      return false;
+    },
+    atrule: function atruleFunc(atrule, atruleName) {
+      var cached, idx, prefix, prefixed;
+
+      cached = atruleFunc.cached;
+
+      if (!cached) {
+        cached = atruleFunc.cached = {};
+      } else if (cached[atrule]) {
+        return cached[atrule];
+      }
+
+      for (idx = 0; idx < prefixesLen; idx += 1) {
+        prefix = prefixes[idx];
+        prefixed = atrule.replace(/^@/, '@' + prefix);
+
+        style.textContent = prefixed + '{}';
+
+        if (style.sheet.cssRules.length > 0) {
+          if (cached[atruleName] === void 0) {
+            cached[atruleName] = atruleName.replace(/^@/, '@' + prefix);
+          }
+
+          cached[atrule] = prefixed;
+
+          return prefixed;
+        }
+      }
+
+      cached[atrule] = false;
+
+      return false;
+    },
+    mq: function mqFunc(mq, mqName) {
+      var cached, idx, prefix, prefixed, mql;
+
+      cached = mqFunc.cached;
+
+      if (!cached) {
+        cached = mqFunc.cached = {};
+      } else if (cached[mq]) {
+        return cached[mq];
+      }
+
+      for (idx = 0; idx < prefixesLen; idx += 1) {
+        prefix = prefixes[idx];
+        prefixed = mq.replace(/\(/g, '(' + prefix);
+        mql = win.matchMedia(prefixed);
+
+        if (mql.media !== 'invalid' && mql.matches) {
+          if (cached[mqName] === void 0) {
+            cached[mqName] = prefix + mqName;
+          }
+
+          cached[mq] = prefixed;
+
+          return prefixed;
+        }
+      }
+
+      cached[mq] = false;
+
+      return false;
+    }
+  };
+}(window, document));
