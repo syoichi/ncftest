83d7f19a7fdd9d4d31aa38fba224d64823b760b1
Syoichi Tsuyuhara <syoichi@outlook.com>
2014-05-26 08:31:22 +0900

    Update "Counter Styles"

--- js/specs.js
+++ js/specs.js
@@ -1630,14 +1630,17 @@
           '‣', '◰ ◳ ◲ ◱', '* ⁑ † ‡',
           'A B C D E F G H I J K L M \nN O P Q R S T U V W X Y Z',
           '\'0\' \'1\' \'2\' \'3\' \'4\' \'5\' \'6\' \'7\' \'8\' \'9\'',
-          'ⓐ ⓑ ⓒ ⓓ ⓔ ⓕ ⓖ ⓗ ⓘ ⓙ ⓚ ⓛ ⓜ ⓝ ⓞ ⓟ ⓠ ⓡ ⓢ ⓣ ⓤ ⓥ ⓦ ⓧ ⓨ ⓩ',
+          'ⓐ ⓑ ⓒ ⓓ ⓔ ⓕ ⓖ ⓗ ⓘ ⓙ ⓚ ⓛ ' +
+            'ⓜ ⓝ ⓞ ⓟ ⓠ ⓡ ⓢ ⓣ ⓤ ⓥ ⓦ ⓧ ⓨ ⓩ',
           '\'1\' linear-gradient(white, black) あ'
         ]),
         'additive-symbols': ['0'].amp(symbol).concat([
           '3 \'a\', 2 \'b\'', '6 ⚅, 5 ⚄, 4 ⚃, 3 ⚂, 2 ⚁, 1 ⚀',
           '\'0\' 0, 1 radial-gradient(white, black), 2 A'
         ]),
-        'speak-as': ['auto', 'numeric', 'alphabetic', 'bullet', 'triangle']
+        'speak-as': [
+          'auto', 'bullets', 'numbers', 'words', 'spell-out', 'triangle'
+        ]
       }
     },
 
